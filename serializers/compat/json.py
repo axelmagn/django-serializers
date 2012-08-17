2e73efe032ba5f7dabdb0164bf0ca6e9c36ee34a
Tom Christie <tom@tomchristie.com>
2012-08-17 14:07:34 +0100

    Update compat wrappers so no hacks on django core needed to run Django's exsiting deserializer tests

--- serializers/compat/json.py
+++ serializers/compat/json.py
@@ -1,11 +1,12 @@
 from serializers import FixtureSerializer
 
+format = 'json'
+
 
 class Serializer(FixtureSerializer):
     def serialize(self, *args, **kwargs):
-        return super(Serializer, self).serialize('json', *args, **kwargs)
+        return super(Serializer, self).serialize(format, *args, **kwargs)
 
-    def deserialize(self, *args, **kwargs):
-        return super(Serializer, self).deserialize('json', *args, **kwargs)
 
-Deserializer = Serializer
+def Deserializer(*args, **kwargs):
+    return Serializer().deserialize(format, *args, **kwargs)
--- serializers/compat/python.py
+++ serializers/compat/python.py
@@ -1,13 +1,14 @@
 from serializers import FixtureSerializer
 
+format = 'python'
+
 
 class Serializer(FixtureSerializer):
     internal_use_only = True  # Backwards compatability
 
     def serialize(self, *args, **kwargs):
-        return super(Serializer, self).serialize('python', *args, **kwargs)
+        return super(Serializer, self).serialize(format, *args, **kwargs)
 
-    def deserialize(self, *args, **kwargs):
-        return super(Serializer, self).deserialize('python', *args, **kwargs)
 
-Deserializer = Serializer
+def Deserializer(*args, **kwargs):
+    return Serializer().deserialize(format, *args, **kwargs)
